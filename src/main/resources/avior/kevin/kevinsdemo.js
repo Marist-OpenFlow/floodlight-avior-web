989c9dbdc97c1398e45a6ef2720fc9d4f2f3cb25
kpietrow <kpietrow@gmail.com>
2013-06-24 13:24:25 -0400

    Created a separate folder for myself

--- src/main/resources/avior/kevin/kevinsdemo.js
+++ src/main/resources/avior/kevin/kevinsdemo.js
@@ -1,4 +1,4 @@
-var Switches = Backbone.Model.extend({
+var Flows = Backbone.Model.extend({
 	urlRoot: '/wm/staticflowentrypusher/json',
 	defaults:{
 		switch:'',
@@ -21,36 +21,61 @@ var Switches = Backbone.Model.extend({
 	}
 });
 
-var SwitchesCollection = Backbone.Collection.extend({
-  	model: Switches,
+var Switches = Backbone.Model.extend({
+	urlRoot:'/wm/core/controller/switches/json',
+	defaults:{
+		dpid:'',
+		actions:'',
+		attributes:'',
+		inetAddress:'',
+		connectedsince:'',
+		ports:'',
+		buffers:'',
+		capabilities:'',
+		harole:''
+	}
+});
+
+var FlowsCollection = Backbone.Collection.extend({
+  	model: Flows,
   	url: '/wm/staticflowentrypusher/list/all/json'
+});
+
+var SwitchesCollection = Backbone.Collection.extend({
+	model:Switches,
 });
 
 //var todos = new SwitchesCollection();
 
 
 var SwitchesView = Backbone.View.extend({
+	el: $('body'),
+	events: {
+		"click button#switch_button": "getSwitches",
+		"click input#clear_button": "clearScreen"
+	},
+	
 	initialize: function(){
+		_.bindAll(this, 'render', 'getSwitches');
+		
+		this.collection = new List()
+		this.collection.bind('switch_button', this.append
+		
 		this.render();
 	},
-
+	
 	render: function(){
-		var template = _.template( $("#switch_template").html(), {} );
-		this.$el.html( template );
+		var self = this;
+		$(this.el).append("<input type='button' id='switch_button'>Get Switches</button>");
 	},
-
-	events: {
-		"click input[id=switch_button]": "getSwitches",
-		"click input[id=clear_button]": "clearScreen"
-	},
-
+	
 	getSwitches: function(event){
 		//var getSwitch = todos.fetch().complete(function () {
 		var getSwitch = this.todos.fetch().complete(function () {
     	  		$( '#container' ).html( JSON.stringify(getSwitch) );
     	 	});
 	},
-
+	
 	clearScreen: function(event){
 		$("#container").html(" ");
 	}
@@ -88,16 +113,16 @@ var FlowView = Backbone.View.extend({
 	initialize: function(){
 		this.render();
 	},
-
+	
 	render: function(){
 		var template = _.template( $("#flow_template").html(), {} );
 		this.$el.html( template );
 	},
-
+	
 	events: {
 		"click input[id=flows_button]": "pushFlows"
 	},
-
+	
 	pushFlows: function(event){
 		var flows = todos.create({
 			switch:'00:00:00:00:00:00:00:01',
@@ -121,7 +146,7 @@ var flow_view = new FlowView({ el: $("#flow_container") });
 	name:'maybe',
 	actions:'output=3',
 });*/
-
+	
 //to delete flows ( REST API DELETE Requests) 
 /*$.ajax({
     type: "DELETE",
@@ -131,3 +156,4 @@ var flow_view = new FlowView({ el: $("#flow_container") });
         console.log(msg);
     }
 });*/
+
