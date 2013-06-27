bb876a98fc0593b7250c1ac483b20be7cfd99d59
kpietrow <kpietrow@gmail.com>
2013-06-27 14:04:25 -0400

    Setting up my side of the project, and working to make it modular

---
+++ src/main/resources/avior/kevin/js/view/memory.js
@@ -0,0 +1,29 @@
+define([
+	"jquery",
+	"underscore",
+	"backbone",
+	"text!template/controller.html"
+], function($, _, Backbone, ctrlTpl){
+	var MemoryView = Backbone.View.extend({
+		//... is a div tag.
+	    tagName:  "div",
+	    // Cache the template function for a single item.
+		//template: _.template($('#controller-template').html()),
+		template: _.template(ctrlTpl),
+		initialize: function(){
+			this.listenTo(this.model, "change", this.render);
+			this.listenTo(this.model, "destroy", this.remove);
+		},
+		events: {
+			"click button": "refresh",
+		},
+		// Re-render the titles of the todo item.
+	    render: function() {
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+	    },
+		refresh: function(){this.model.fetch();}
+	});
+	return MemoryView;
+});
+
