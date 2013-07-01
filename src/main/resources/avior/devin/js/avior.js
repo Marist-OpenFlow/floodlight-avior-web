f4b890fa621fdb56776dec6b2b6dbf12288c67b7
DevinYoung <devin.young1@marist.edu>
2013-07-01 11:28:50 -0400

    modified avior.js

--- src/main/resources/avior/devin/js/avior.js
+++ src/main/resources/avior/devin/js/avior.js
@@ -2,21 +2,21 @@ define([
 	"jquery",
 	"underscore",
 	"backbone",
-	"floodlight/switchSumFL",
-	"view/switchesSumView"
-], function($, _, Backbone, SwitchSumFL, SwitchesSumView){
+	"floodlight/switch",
+	"view/switches"
+], function($, _, Backbone, Switch, SwitchesView){
 	return {
-		SwitchSumFL: SwitchSumFL,
-		SwitchesSumView: SwitchesSumView,
+		Switch: Switch,
+		SwitchesView: SwitchesView,
 		initialize: function(){
 			$(document).ready(function(){
-				/*var switchesview = new SwitchesView({model: new Switch});
+				var switchesview = new SwitchesView({model: new Switch});
 				//$(document.body).append(switchesview.render().el);
-				switchesview.delegateEvents(switchesview.events);*/
+				switchesview.delegateEvents(switchesview.events);
 				
-				var switchesSumView = new SwitchesSumView({model: new SwitchSumFL});
+				/*var switchesSumView = new SwitchesSumView({model: new SwitchSumFL});
 				//$(document.body).append(switchesSumView.render().el);
-				switchesSumView.delegateEvents(switchesSumView.events);
+				switchesSumView.delegateEvents(switchesSumView.events);*/
 			});
 		}
 	};
