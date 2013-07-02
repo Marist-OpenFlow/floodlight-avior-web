d57326c83a73160559cb03d8d50d5860d0b8b88f
Devin Young <devin.young1@marist.edu>
2013-07-02 16:41:26 -0400

    created a port submodel within the switch model

--- src/main/resources/avior/devin/js/model/switch.js
+++ src/main/resources/avior/devin/js/model/switch.js
@@ -4,18 +4,25 @@ define([
 	"openflow",
 	"model/portModel",
 	"model/descriptionModel"
-], function(Backbone,Util, Port, Description){
+], function(Backbone, Util, Port, Description){
 	
 	/* Structure to hold switch information */
 	var Switch = Backbone.Model.extend({
+		model1: Port,
+		model2: Description,
+		
 		parse: function (resp) {
         		return {
-            		ports: resp.ports[0],
+            		ports: this.getPortArray(resp.ports),
             		description: resp.description,
             		inetAddress: resp.inetAddress,
             		connectedSince: resp.connectedSince,
             		dpid: resp.dpid
         		}
+        },
+        
+        getPortArray: function(resp) {
+        	alert(resp);
     	}
 	});
 	return Switch;
