eb563a1f82e2990420f81c4ae905271f8a5079b4
kpietrow <kpietrow@gmail.com>
2013-07-19 16:55:10 -0400

    Have text for Modulestpl. Will be fine tuning it

--- src/main/resources/avior/kevin/js/model/controller/modulesmodel.js
+++ src/main/resources/avior/kevin/js/model/controller/modulesmodel.js
@@ -6,16 +6,12 @@ define([
 	/* Structure to hold controller metadata */
 	var ModulesModel = Backbone.Model.extend({
 		urlRoot: Util.missingCtlrErr,
+		
 		defaults: {
 			modules: [],
 			moduleText: ''
 		},
 		
-		initialize: function(data){
-			var self = this;
-			self.set({modules:_.keys(data)});
-            self.set({moduleText:_.reduce(_.keys(data), function(s, m) {return s+m.replace("net.floodlightcontroller", "n.f")+", "}, '')});
-		},
 	});
 	return ModulesModel;
 });
