8068a3da093883558636ca8b7594ba867820c72d
DevinYoung <devin.young1@marist.edu>
2013-08-01 10:56:40 -0400

    integrating page navigation

--- src/main/resources/avior/test/js/avior.js
+++ src/main/resources/avior/test/js/avior.js
@@ -12,8 +12,7 @@ define([
 	"view/uptimeview",
 	"router/router",
 ], function($, _, Backbone, Memory, Modules, Status, Uptime, MemoryView, ModulesView, StatusView, UptimeView, Router){
-	return {
-		/* 
+	return { 
 		Memory: Memory,
 		Modules: Modules,
 		Status: Status, 
@@ -26,36 +25,29 @@ define([
 		
 		initialize: function(){
 			$(document).ready(function(){
-				/* Create views for the controller */
+				//Create views for the controller 
 				var statusview = new StatusView({model: new Status});
 				var uptimeview = new UptimeView({model: new Uptime});
 				var memoryview = new MemoryView({model: new Memory});
 				var modulesview = new ModulesView({model: new Modules});
 				
-				/* Delegate events for controller views */
+				//Delegate events for controller views
 				statusview.delegateEvents(statusview.events);
 				uptimeview.delegateEvents(uptimeview.events);
 				memoryview.delegateEvents(memoryview.events);
 				modulesview.delegateEvents(modulesview.events);
 				
-				/* Link controller aspects to id tags */
+				//Link controller aspects to id tags
 				$('#uptimeview').append(uptimeview.render().el);
 				$('#statusview').append(statusview.render().el);
 				$('#memoryview').append(memoryview.render().el);
 				$('#modulesview').append(modulesview.render().el);
 				
+				//$.mobile.linkBindingEnabled = false;
+    			//$.mobile.hashListeningEnabled = false;
 				var router = new Router();
 				Backbone.history.start();
 				
-				/*if ( history && history.pushState ) {
-    				Backbone.history.start({pushState: true});
-    				console.log('has pushState');
-				}
-				else {
-    				Backbone.history.start();
-    				console.log('no pushState');
-				}*/
-
 			
 				/*
 				var flowview = new FlowView({model: new Flow});
--- src/main/resources/avior/test/js/router/router.js
+++ src/main/resources/avior/test/js/router/router.js
@@ -5,18 +5,49 @@ define([
 	"marionette",
 	"floodlight/switch",
 	"view/switchDetail",
-], function($, _, Backbone, Marionette, Switch, SwitchDetail){
+	"floodlight/memory",
+	"floodlight/modules",
+	"floodlight/status",
+	"floodlight/uptime",
+	"view/memoryview",
+	"view/modulesview",
+	"view/statusview",
+	"view/uptimeview",
+], function($, _, Backbone, Marionette, Switch, SwitchDetail, Memory, Modules, Status, Uptime, MemoryView, ModulesView, StatusView, UptimeView){
 	/* Structure used to navigate through views */
 	var Router = Marionette.AppRouter.extend({
 		routes: {
+			//"": "home",
 			"switches": "switchRoute",
 		},
-
+		
+		/* home: function() {
+			this.statusview = new StatusView({model: new Status});
+			this.uptimeview = new UptimeView({model: new Uptime});
+			this.memoryview = new MemoryView({model: new Memory});
+			this.modulesview = new ModulesView({model: new Modules});
+				
+			//Delegate events for controller views
+			this.statusview.delegateEvents(statusview.events);
+			this.uptimeview.delegateEvents(uptimeview.events);
+			memoryview.delegateEvents(memoryview.events);
+			modulesview.delegateEvents(modulesview.events);
+				
+			//Link controller aspects to id tags
+			$('#uptimeview').append(this.uptimeview.render().el);
+			$('#statusview').append(this.statusview.render().el);
+			$('#memoryview').append(this.memoryview.render().el);
+			$('#modulesview').append(this.modulesview.render().el);
+        },
+        */
+        
+		
 		switchRoute: function() {
-			//console.log("router routing routers");
+			console.log("router routing routers");
 			var switchDetail = new SwitchDetail({model: new Switch});
 			switchDetail.delegateEvents(switchDetail.events);
 		},
+
 	});
 	return Router;
 }); 
